85761493a3316d131cfed7b1e897c9cc24397cd4
Brikshya41 <brikshyadhungana@gmail.com>
2017-12-03 15:51:31 +0545

    assignment of dec1

--- nov_30.py
+++ nov_30.py
@@ -1,7 +1,4 @@
-def LHS (a,b):
-    return (a+b)**2
-def RHS(a,b):
-    return a**2+2*a*b+b**2
-l=LHS(2,3)
-r=RHS(2,3)
-print('LHS={0} RHS={1}'.format(l,r))
+def singasongfor(nameoffriend = "ok"):
+    print("{0} {0} {1}".format("happy birthday to you" , nameoffriend))
+
+singasongfor("aashika")
